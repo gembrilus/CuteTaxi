475c875ec9e25a7966965ea53cb75f23344141d8
Максим Наконечный <gembrilus@gmail.com>
2020-02-04 07:43:02 +0200

    Fix custom PreferenceDataStre for store preferences to DB and to sharedPreferences

--- app/src/main/java/ua/com/cuteteam/cutetaxiproject/settings/AppSettingsToFirebaseStore.kt
+++ app/src/main/java/ua/com/cuteteam/cutetaxiproject/settings/AppSettingsToFirebaseStore.kt
@@ -11,12 +11,13 @@ private const val ERROR_MESSAGE_GET = "You need to set a 'get' function first. "
  * Class AppSettingsToFirebaseStore stores and/or reads values of shared header_preferences in a file on the device and
  * send changes into the Firebase database
  */
-class AppSettingsToFirebaseStore: AppSettingsStore() {
+class AppSettingsToFirebaseStore : AppSettingsStore() {
 
     private var _put: ((String, Array<String>) -> Unit)? = null
-    private val put get() = _put
-        ?.let { it }
-        ?: throw IllegalArgumentException(ERROR_MESSAGE_PUT)
+    private val put
+        get() = _put
+            ?.let { it }
+            ?: throw IllegalArgumentException(ERROR_MESSAGE_PUT)
 
     /**
      * Sets a function that puts values into the database.
@@ -25,14 +26,15 @@ class AppSettingsToFirebaseStore: AppSettingsStore() {
      * and various parameters (or one) and put them to database
      *
      */
-    fun setPutFunction(put: (String, Array<String>) -> Unit){
+    fun setPutFunction(put: (String, Array<String>) -> Unit) {
         _put = put
     }
 
     private var _get: ((String) -> Array<String>)? = null
-    private val get get() = _get
-        ?.let { it }
-        ?: throw IllegalArgumentException(ERROR_MESSAGE_GET)
+    private val get
+        get() = _get
+            ?.let { it }
+            ?: throw IllegalArgumentException(ERROR_MESSAGE_GET)
 
 
     /**
@@ -42,28 +44,41 @@ class AppSettingsToFirebaseStore: AppSettingsStore() {
      * and return array of values (or one value only) from the database
      *
      */
-    fun setGetFunction(get: (String) -> Array<String>){
+    fun setGetFunction(get: (String) -> Array<String>) {
         _get = get
     }
 
 
-    override fun getString(key: String?, defValue: String?): String = key?.let { get(it)[0] } ?: defValue ?: ""
+    override fun getString(key: String?, defValue: String?): String =
+        super.getString(key, defValue)
+            ?: key?.let { get(it)[0] }
+            ?: defValue
+            ?: ""
 
 
-    override fun putString(key: String?, value: String?) = key?.let {k ->
-        value?.let {v ->
-            put(k, arrayOf(v))
-        }
-    } ?: Unit
+    override fun putString(key: String?, value: String?) {
+        super.putString(key, value)
+        key?.let { k ->
+            value?.let { v ->
+                put(k, arrayOf(v))
+            }
+        } ?: Unit
+    }
 
 
-    override fun putStringSet(key: String?, values: MutableSet<String>?) = key?.let {k ->
-        values?.let { v ->
-            put(k, v.toTypedArray())
-        }
-    } ?: Unit
+    override fun putStringSet(key: String?, values: MutableSet<String>?) {
+        super.putStringSet(key, values)
+        key?.let { k ->
+            values?.let { v ->
+                put(k, v.toTypedArray())
+            }
+        } ?: Unit
+    }
 
 
     override fun getStringSet(key: String?, defValues: MutableSet<String>?): MutableSet<String> =
-        key?.let { get(it).toMutableSet() } ?: defValues ?: mutableSetOf()
+        super.getStringSet(key, defValues)
+            ?: key?.let { get(it).toMutableSet() }
+            ?: defValues
+            ?: mutableSetOf()
 }
