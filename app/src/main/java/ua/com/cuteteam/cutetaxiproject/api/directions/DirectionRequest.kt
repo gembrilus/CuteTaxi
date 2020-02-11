a7886b26aa8ee6395e36bce67b096bcb95982dc5
Максим Наконечный <gembrilus@gmail.com>
2020-02-11 20:33:41 +0200

    Add Builders to ApiServices, enable Chucker

--- app/src/main/java/ua/com/cuteteam/cutetaxiproject/api/directions/DirectionRequest.kt
+++ app/src/main/java/ua/com/cuteteam/cutetaxiproject/api/directions/DirectionRequest.kt
@@ -3,13 +3,88 @@ package ua.com.cuteteam.cutetaxiproject.api.directions
 import kotlinx.coroutines.withContext
 import ua.com.cuteteam.cutetaxiproject.BuildConfig
 import ua.com.cuteteam.cutetaxiproject.api.APIRequest
+import java.util.*
 
 class DirectionRequest: APIRequest<DirectionService>() {
 
     override val url: String
         get() = BuildConfig.GOOGLE_DIRECTIONS_API_URL
 
-    suspend fun requestDirection(map: Map<String, String>) = withContext(IO){
+    suspend fun fullDirectionRequest(map: Map<String, String>) = withContext(IO){
         getService<DirectionService>().getDirection(map)
+    }
+
+    suspend fun simpleDirectionRequest(orig: String, dest: String): Route = withContext(IO) {
+        val map = Builder()
+            .addOrigin(orig)
+            .addDestination(dest)
+            .build()
+        fullDirectionRequest(map)
+    }
+
+
+    class Builder {
+        private lateinit var origin: String
+        private lateinit var destination: String
+        private var wayPoints: MutableList<String> = mutableListOf()
+        private var alternatives: Boolean = false
+        private var avoid: MutableSet<String> = mutableSetOf()
+        private var language = Locale.getDefault().language
+        private var units = Units.METRIC
+        private var region = Locale.getDefault().country.toLowerCase(Locale.ENGLISH)
+
+
+        fun addOrigin(origin: String) = apply {
+            this@Builder.origin = origin
+        }
+
+        fun addDestination(dest: String) = apply {
+            destination = dest
+        }
+
+        fun enableAlternatives() = apply {
+            alternatives = true
+        }
+
+        fun addWayPoint(point: String) = apply {
+            wayPoints.add(point)
+        }
+
+        fun addAvoid(_avoid: String) = apply {
+            avoid.add(_avoid)
+        }
+
+        fun setLanguage(lang: String) = apply {
+            language = lang
+        }
+
+        fun setMeasureSystem(system: String) = apply {
+            units = system
+        }
+
+        fun setRegion(country: String) = apply {
+            region = country
+        }
+
+        fun build(): Map<String, String> {
+            val map = mutableMapOf<String, String>()
+
+            map[RequestParameters.ORIGIN_PLACE] = origin
+            map[RequestParameters.DESTINATION_PLACE] = destination
+            if (wayPoints.isNotEmpty()) {
+                map[RequestParameters.WAY_POINTS] = wayPoints.joinToString("|", "", "")
+            }
+            if (alternatives) {
+                map[RequestParameters.IS_ALTERNATIVE_WAYS] = alternatives.toString()
+            }
+            if (avoid.isNotEmpty()) {
+                map[RequestParameters.AVOID] = avoid.joinToString("|", "", "")
+            }
+            map[RequestParameters.LANG] = language
+            map[RequestParameters.UNITS] = units
+            map[RequestParameters.REGION] = region
+
+            return map
+        }
     }
 }
--- app/src/main/java/ua/com/cuteteam/cutetaxiproject/api/geocoding/GeocodingService.kt
+++ app/src/main/java/ua/com/cuteteam/cutetaxiproject/api/geocoding/GeocodingService.kt
@@ -2,12 +2,21 @@ package ua.com.cuteteam.cutetaxiproject.api.geocoding
 
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.QueryMap
 import ua.com.cuteteam.cutetaxiproject.api.APIService
 
 interface GeocodeService : APIService {
-    @GET
-    suspend fun getNameByCoordinates(@Query("latlng") latlng: String): Geocode
 
     @GET
-    suspend fun getCoordinatesByName(@Query("address", encoded = true) address: String): Geocode
+    suspend fun getNameByCoordinates(
+        @Query("latlng") latlng: String,
+        @QueryMap map: Map<String, String>
+    ): Geocode
+
+    @GET
+    suspend fun getCoordinatesByName(
+        @Query("address", encoded = true) address: String,
+        @QueryMap map: Map<String, String>
+    ): Geocode
+
 }
