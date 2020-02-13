369aa35b456bf5c3476a8445cb0f54225c55b38b
Максим Наконечный <gembrilus@gmail.com>
2020-02-13 12:04:35 +0200

    Refactor builders, change API Key, add new method to RouteBuilder

--- app/src/main/java/ua/com/cuteteam/cutetaxiproject/api/geocoding/GeocodeRequest.kt
+++ app/src/main/java/ua/com/cuteteam/cutetaxiproject/api/geocoding/GeocodeRequest.kt
@@ -1,41 +1,31 @@
 package ua.com.cuteteam.cutetaxiproject.api.geocoding
 
-import kotlinx.coroutines.withContext
 import ua.com.cuteteam.cutetaxiproject.BuildConfig
 import ua.com.cuteteam.cutetaxiproject.api.APIRequest
 import ua.com.cuteteam.cutetaxiproject.api.RequestParameters
 import java.util.*
 
-class GeocodeRequest : APIRequest<GeocodeService>() {
+class GeocodeRequest() : APIRequest<GeocodeService>() {
+
+    internal constructor(map: Map<String, String>): this(){
+        this.map = map
+    }
+
+    private lateinit var map: Map<String, String>
 
     override val url: String
         get() = BuildConfig.GOOGLE_GEO_API_URL
 
-    suspend fun fullRequestNameByCoordinates(lat: Double, lon: Double, map: Map<String, String>) =
-        withContext(IO) {
-            getService<GeocodeService>().getNameByCoordinates("$lat, $lon", map)
-        }
-
-    suspend fun fullRequestCoordinatesByName(address: String, map: Map<String, String>) =
-        withContext(IO) {
-            getService<GeocodeService>().getCoordinatesByName(address, map)
-        }
-
-    suspend fun requestNameByCoordinates(lat: Double, lon: Double) = withContext(IO) {
-        val map = Builder().build()
+    suspend fun requestNameByCoordinates(lat: Double, lon: Double) =
         getService<GeocodeService>().getNameByCoordinates("$lat, $lon", map)
-    }
 
-    suspend fun requestCoordinatesByName(address: String) = withContext(IO) {
-        val map = Builder().build()
+    suspend fun requestCoordinatesByName(address: String) =
         getService<GeocodeService>().getCoordinatesByName(address, map)
-    }
-
 
     class Builder {
 
         private var language: String = Locale.getDefault().language
-        private var region: String = Locale.getDefault().country.toLowerCase(Locale.ENGLISH)
+        private var region: String = ""
 
         fun setLanguageResponse(lang: String) = apply {
             language = lang
@@ -45,10 +35,14 @@ class GeocodeRequest : APIRequest<GeocodeService>() {
             region = _region
         }
 
-        fun build(): Map<String, String> = mapOf(
-            RequestParameters.LANG to language,
-            RequestParameters.REGION to region
-        )
+        fun build(): GeocodeRequest {
+            val map = mutableMapOf<String, String>()
+            map[RequestParameters.LANG] = language
+            if (region.isNotEmpty()){
+                map[RequestParameters.REGION] = region
+            }
+            return GeocodeRequest(map)
+        }
 
     }
 
