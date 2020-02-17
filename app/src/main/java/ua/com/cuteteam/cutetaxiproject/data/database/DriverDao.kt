17a016b672475c3e30e44d6ae803c4a03011ecb9
yuriy-burkut <burkut89@gmail.com>
2020-02-17 21:28:18 +0200

    modified some entities, classes for working with realtime database was renamed

--- app/src/main/java/ua/com/cuteteam/cutetaxiproject/data/database/DriverDao.kt
+++ app/src/main/java/ua/com/cuteteam/cutetaxiproject/data/database/DriverDao.kt
@@ -3,7 +3,7 @@ package ua.com.cuteteam.cutetaxiproject.data.database
 import com.google.firebase.database.DatabaseReference
 import ua.com.cuteteam.cutetaxiproject.data.entities.Trip
 
-class DriverDbHelper : DbHelper() {
+class DriverDao : BaseDao() {
 
     override val usersReference: DatabaseReference
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
--- app/src/main/java/ua/com/cuteteam/cutetaxiproject/data/entities/Order.kt
+++ app/src/main/java/ua/com/cuteteam/cutetaxiproject/data/entities/Order.kt
@@ -1,7 +1,13 @@
 package ua.com.cuteteam.cutetaxiproject.data.entities
 
-class Order {
-
-
-
-}
+data class Order (
+    var orderId: String? = null,
+    var driverLocation: Coordinates? = null,
+    var comfortLevel: ComfortLevel = ComfortLevel.STANDARD,
+    var coordinatesStart: Coordinates? = null,
+    var coordinatesFinish: Coordinates? = null,
+    var addressStart: String? = null,
+    var addressDestination: String? = null,
+    var distance: Double? = null,
+    var price: Double? = null
+)
