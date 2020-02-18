50f7adac62b87258968044e15620976b05a6bcc7
yuriy-burkut <burkut89@gmail.com>
2020-02-18 16:06:26 +0200

    add function for subscrubing for changes, modify entities and entries

--- app/src/main/java/ua/com/cuteteam/cutetaxiproject/data/database/DriverDao.kt
+++ app/src/main/java/ua/com/cuteteam/cutetaxiproject/data/database/DriverDao.kt
@@ -1,14 +1,18 @@
 package ua.com.cuteteam.cutetaxiproject.data.database
 
 import com.google.firebase.database.DatabaseReference
-import ua.com.cuteteam.cutetaxiproject.data.entities.Trip
+import ua.com.cuteteam.cutetaxiproject.data.User
+import ua.com.cuteteam.cutetaxiproject.data.entities.Driver
+import ua.com.cuteteam.cutetaxiproject.extentions.getValue
 
 class DriverDao : BaseDao() {
 
-    override val usersReference: DatabaseReference
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val usersRef: DatabaseReference
+        get() = rootRef.child("drivers")
 
-    override fun getTrips(): List<Trip> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override suspend fun getUser(uid: String): User? {
+        val userData = usersRef.child(uid).getValue()
+        return userData.getValue(Driver::class.java)
     }
+
 }
