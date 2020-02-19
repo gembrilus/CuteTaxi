ac517c0b909d9d4737d54f34974cddba3676a9a6
Camila-1 <camilaabalova@gmail.com>
2020-02-20 00:39:38 +0200

    handle permanently denied  permissions

--- app/src/main/java/ua/com/cuteteam/cutetaxiproject/PermissionProvider.kt
+++ app/src/main/java/ua/com/cuteteam/cutetaxiproject/PermissionProvider.kt
@@ -1,23 +1,36 @@
 package ua.com.cuteteam.cutetaxiproject
 
-import android.app.Activity
 import pub.devrel.easypermissions.EasyPermissions
 import android.Manifest
+import android.content.Context
+import androidx.fragment.app.FragmentActivity
+import ua.com.cuteteam.cutetaxiproject.application.AppClass
+import ua.com.cuteteam.cutetaxiproject.common.dialogs.InfoDialog
 
-open class Permission(open val rationale: String, val permission: String, val requestCode: Int)
-data class AccessFineLocationPermission(override val rationale: String) : Permission(
-    rationale,
+open class Permission(
+    open val rationale: String, val name: String, val requestCode: Int,
+    val requiredPermissionDialogTitle: String,
+    val requiredPermissionDialogMessage: String
+)
+
+data class AccessFineLocationPermission(val context: Context = AppClass.appContext()) : Permission(
+    context.getString(R.string.call_phone_permission_rationale),
     Manifest.permission.ACCESS_FINE_LOCATION,
-    PermissionProvider.LOCATION_REQUEST_CODE
+    PermissionProvider.LOCATION_REQUEST_CODE,
+    context.getString(R.string.location_permission_required_dialog_title),
+    context.getString(R.string.location_permission_required_dialog_message)
 )
 
-data class CallPhonePermission(override val rationale: String) : Permission(
-    rationale,
+data class CallPhonePermission(val context: Context = AppClass.appContext()) : Permission(
+    context.getString(R.string.location_permission_rationale),
     Manifest.permission.CALL_PHONE,
-    PermissionProvider.CALL_PHONE_REQUEST_CODE
+    PermissionProvider.CALL_PHONE_REQUEST_CODE,
+    context.getString(R.string.call_phone_permission_required_dialog_title),
+    context.getString(R.string.call_phone_permission_required_dialog_message)
 )
 
-class PermissionProvider(private val activity: Activity) : EasyPermissions.PermissionCallbacks {
+class PermissionProvider(private val activity: FragmentActivity) :
+    EasyPermissions.PermissionCallbacks {
 
     companion object {
         const val LOCATION_REQUEST_CODE = 1
@@ -27,14 +40,33 @@ class PermissionProvider(private val activity: Activity) : EasyPermissions.Permi
     var onGranted: (() -> Unit)? = null
 
     fun withPermission(permission: Permission, callback: () -> Unit) {
-        if (EasyPermissions.hasPermissions(activity, permission.permission))
+        if (EasyPermissions.hasPermissions(activity, permission.name)) {
             callback()
-        else EasyPermissions.requestPermissions(
+        } else {
+            requestPermission(permission)
+        }
+    }
+
+    private fun requestPermission(permission: Permission) {
+        val isPermanentlyDenied = EasyPermissions.somePermissionPermanentlyDenied(
             activity,
-            permission.rationale,
-            permission.requestCode,
-            permission.permission
+            listOf(permission.name)
         )
+
+        if (isPermanentlyDenied) {
+            InfoDialog.show(
+                activity.supportFragmentManager,
+                permission.requiredPermissionDialogTitle,
+                permission.requiredPermissionDialogMessage
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                activity,
+                permission.rationale,
+                permission.requestCode,
+                permission.name
+            )
+        }
     }
 
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {}
