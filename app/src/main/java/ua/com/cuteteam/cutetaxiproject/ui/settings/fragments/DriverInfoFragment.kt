29b5518c17fba2226d8c6e417d9ac6d2f06157e2
Максим Наконечный <gembrilus@gmail.com>
2020-03-09 00:43:44 +0200

    Refactored settings. Removed SPKeys because there are used DbEntries Add back menu item to Navigation for Settings

--- app/src/main/java/ua/com/cuteteam/cutetaxiproject/ui/settings/fragments/DriverInfoFragment.kt
+++ app/src/main/java/ua/com/cuteteam/cutetaxiproject/ui/settings/fragments/DriverInfoFragment.kt
@@ -1,6 +1,10 @@
 package ua.com.cuteteam.cutetaxiproject.ui.settings.fragments
 
+import androidx.preference.Preference
 import ua.com.cuteteam.cutetaxiproject.R
+import ua.com.cuteteam.cutetaxiproject.data.database.DbEntries
+import ua.com.cuteteam.cutetaxiproject.data.database.DriverDao
+import ua.com.cuteteam.cutetaxiproject.shPref.FirebaseSettingsDataStore
 
 private const val TAG = "CuteTaxi.AddInfoFrag"
 
@@ -9,13 +13,23 @@ class DriverInfoFragment : BaseSettingsFragment() {
     override val resourceId: Int
         get() = R.xml.driver_info_preferences
 
-    val changedStore by lazy {
-        listOf(
-            spKeys.PASSENGER_CAR_CLASS_KEY
+    private val appSettingsToFirebaseStore by lazy {
+        FirebaseSettingsDataStore(
+            shPref,
+            DriverDao()
         )
     }
 
     override fun setNewDataStore() {
-        setDataStore(changedStore, appSettingsToFirebaseStore)
+        findPreference<Preference>(DbEntries.Car.BRAND)
+            ?.preferenceDataStore = appSettingsToFirebaseStore
+        findPreference<Preference>(DbEntries.Car.MODEL)
+            ?.preferenceDataStore = appSettingsToFirebaseStore
+        findPreference<Preference>(DbEntries.Car.NUMBER)
+            ?.preferenceDataStore = appSettingsToFirebaseStore
+        findPreference<Preference>(DbEntries.Car.COLOR)
+            ?.preferenceDataStore = appSettingsToFirebaseStore
+        findPreference<Preference>(DbEntries.Car.COMFORT_LEVEL)
+            ?.preferenceDataStore = appSettingsToFirebaseStore
     }
 }
