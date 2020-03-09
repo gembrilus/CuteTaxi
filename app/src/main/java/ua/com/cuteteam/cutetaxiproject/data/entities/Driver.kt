55ccddbd18b186db7643455db02483bd84eb197e
yuriy-burkut <burkut89@gmail.com>
2020-03-09 03:43:14 +0200

    merge to master

--- app/src/main/java/ua/com/cuteteam/cutetaxiproject/data/entities/Driver.kt
+++ app/src/main/java/ua/com/cuteteam/cutetaxiproject/data/entities/Driver.kt
@@ -11,7 +11,7 @@ data class Driver(
     override var phoneNumber: String? = "",
     var car: Car? = null,
     override var rate: Double? = 0.0,
-    var status: Status? = Status.OFFLINE,
+    var status: DriverStatus? = DriverStatus.OFFLINE,
     var location: LatLng? = null,
     var orderId: String? = null
 ) : User {
